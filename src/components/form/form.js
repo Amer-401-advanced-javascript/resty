079c55b5853e8a67ceb0c90f84a2674a1d7a7049
Amer-bit <ny.1995@outlook.com>
2020-06-29 01:54:37 +0300

    almostDone

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,47 +1,25 @@
 import React from 'react';
 import './form.scss'
 
-// const Form = () => {
-//     return ( 
-//         <form>
-//             <label className='url'> URL</label>
-//             <input type='text' id='url'/>
-//             <button>GO!</button>
-            
-//             <label for='get'>GET</label>
-//             <input type='radio' name='rest' id='get' value='GET' />
-
-//             <label for='post'>POST</label>
-//             <input type='radio' name='rest' id='post' value='POST' />
-
-//             <label for='put'>PUT</label>
-//             <input type='radio' name='rest' id='put' value='PUT' />
-
-//             <label for='delete'>DELETE</label>
-//             <input type='radio' name='rest' id='delete' value='DELETE' />
-//         </form>
-//     )
-// }
 
 class Form extends React.Component{
     constructor(prop){
         super(prop);
         this.state = {
-            url:'url'
+            url:'',
+            method:''
         }
     }
 
-    handleClick(event){
+    handleClick = (event)=>{
         event.preventDefault();
         let rest = document.getElementsByName('rest')
-        let url = document.getElementById('url')
-        let result = document.getElementById('result')
-        
+        let url = document.getElementById('url').value        
         for( let i =0; i < rest.length; i++){
-            if(rest[i].checked){   
-                return(             
-                    result.innerHTML = <p>{url.value}   {rest[i].value}</p>
-                )
+            if(rest[i].checked){
+                let method = rest[i].value           
+                    this.setState({method})
+                    this.setState({url})
             }
         }
     }
@@ -68,7 +46,7 @@ class Form extends React.Component{
         </form>
         
         <div id='result'>
-
+            <p>{this.state.method} {this.state.url}</p>
         </div>
         </>
         
