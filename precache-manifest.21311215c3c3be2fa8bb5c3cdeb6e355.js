55eeff9a0b16df3d8061c1e577e9806376fd2cfe
Amer-bit <ny.1995@outlook.com>
2020-06-29 15:51:16 +0300

    Updates

---
+++ precache-manifest.21311215c3c3be2fa8bb5c3cdeb6e355.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ecee3a37bbfa28de7e3367b91118c92",
+    "url": "/Amer-401-advanced-javascript/resty/index.html"
+  },
+  {
+    "revision": "dbf95af1c42607f13604",
+    "url": "/Amer-401-advanced-javascript/resty/static/css/main.e48a24e9.chunk.css"
+  },
+  {
+    "revision": "64909891a31834ed1312",
+    "url": "/Amer-401-advanced-javascript/resty/static/js/2.25b57a3a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Amer-401-advanced-javascript/resty/static/js/2.25b57a3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbf95af1c42607f13604",
+    "url": "/Amer-401-advanced-javascript/resty/static/js/main.2866a1ec.chunk.js"
+  },
+  {
+    "revision": "6014348771a7b6f9a524",
+    "url": "/Amer-401-advanced-javascript/resty/static/js/runtime-main.658ff290.js"
+  }
+]);
